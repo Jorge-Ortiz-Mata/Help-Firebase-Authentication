7cc806c322c1699d38f73a8ccf5623013452827c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-11 18:14:22 -0500

    Sign Up usign Firebase.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class PagesController < ApplicationController
+  def index
+  end
+
+  def signup
+    user_email
+    uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCZ-rftIN5eO4SKP41ahr5SCOHetWzjOYA")
+
+    response = Net::HTTP.post_form(uri, "email": @email, "password": @password)
+    data = JSON.parse(response.body)
+    session[:user_id] = data["localId"]
+    session[:data] = data
+
+    redirect_to root_path, notice: "Sign Up successfully." if response.is_a?(Net::HTTPSuccess)
+  end
+
+  def home
+  end
+
+  def login
+  end
+
+  private
+    def user_email
+      @email = params[:email]
+      @password = params[:password]
+      puts @email
+      puts @password
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get '/signup', to: "pages#signup"
+  get '/home', to: "pages#home"
+
+  post '/login', to: "pages#login"
+  post '/logout', to: "pages#logout"
+  post '/signup', to: "pages#signup"
+
+  # post '/create_todo', to: "pages#create_todo"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
 end
